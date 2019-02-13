8ec6bce25e5b9e309eb94cda1a7eebdf24718d5e
wanhenri <slipkorn.jk@gmail.com>
2019-02-13 19:25:49 -0200

    dir js

---
+++ aula013/js/description-product.js
@@ -0,0 +1,15 @@
+/* 
+
+1 - ter a janela (navegador / browser) na mão
+2 - localizar o html
+3 - Pegar o coração
+4 - Pegar clique no coraçao 
+5 - No instante que o usuario clicar, mostraremos um texto 'AE' na tela
+
+*/
+
+alert('Antes do nossoo comando que pega o coração!');
+
+const $heart = window.document.querySelector('.-heart'); 
+
+console.log($heart);
