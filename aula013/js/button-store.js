85ceb54daaeae4be4d9b031e0b1637de43b2c364
wanhenri <slipkorn.jk@gmail.com>
2019-02-13 21:37:43 -0200

    add button desafio 13

---
+++ aula013/js/button-store.js
@@ -0,0 +1,9 @@
+const $buttonSecond = window.document.querySelector('.-second');
+
+// $buttonSecond.addEventListener("click", handleClick);
+
+console.log($buttonSecond)
+
+// function handleClick() {
+//     console.log('Teste');
+// }
