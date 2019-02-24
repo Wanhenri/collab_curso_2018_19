883efc163b7e746c558814a4adba47d8139f794e
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-02-24 18:31:41 -0300

    Update home.js

--- aula014/js/home.js
+++ aula014/js/home.js
@@ -6,13 +6,7 @@ $second.addEventListener("click", handleClick);
 
 function handleClick() {
     const $carrinho = document.querySelector('.-last');
-    // Fazem a mesma coisa;
-    // valorInicial = valorInicial + 1; 
-    // valorInicial += 1;
-    // valorInicial++;
-
-    // template string
-    // $carrinho.textContent = 'Carrinho (' + ++valorInicial + ')';
+ 
     $carrinho.textContent = `Carrinho (${++valorInicial})`;
 }
 
