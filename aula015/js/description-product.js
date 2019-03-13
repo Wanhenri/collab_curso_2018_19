6c88c4436463df7c079d9d051c1005ce4334f518
Wanderson Henrique <slipkorn.jk@gmail.com>
2019-03-13 20:33:00 -0300

    retirando comentarios

--- aula015/js/description-product.js
+++ aula015/js/description-product.js
@@ -1,12 +1,4 @@
-/* 
 
-1 - ter a janela (navegador / browser) na mão
-2 - localizar o html
-3 - Pegar o coração
-4 - Pegar clique no coraçao 
-5 - No instante que o usuario clicar, mostraremos um texto 'AE' na tela
-
-*/
 const $heart = document.querySelector('.-heart'); 
 
 const $stars = document.querySelector(".-star");
