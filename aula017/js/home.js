e98264a366e1d338b62a17382e2525b78cba9564
wanhenri <slipkorn.jk@gmail.com>
2019-06-27 19:16:12 -0300

    teste

--- aula017/js/home.js
+++ aula017/js/home.js
@@ -3,10 +3,8 @@ const $second = document.querySelector(".-second");
 
 $second.addEventListener("click", handleClick);
 
-
 function handleClick() {
     const $carrinho = document.querySelector('.-last');
  
     $carrinho.textContent = `Carrinho (${++valorInicial})`;
 }
-
